358c2981b86430ef8fe495624287d149b0a4b1cc
yb <lojobs38@gmail.com>
2023-03-27 03:10:40 +0800

    Update example

--- examples/selenium.js
+++ examples/selenium.js
@@ -6,28 +6,53 @@ const By = webdriver.By;
 const until = webdriver.until;
 const selProxy = require('selenium-webdriver/proxy');
 const bmpClient = require('../index').createClient();
+const fs = require('fs');
 //need to require this or it looks for a globally installed chromedriver
 const chromedriver = require('chromedriver');
+const { ServiceBuilder, Options } = require('selenium-webdriver/chrome');
+
+let harData;
+let driver;
+// let chromeDriverPath = "/path/to/chromedriver";
+// const serviceBuilder = new ServiceBuilder(chromeDriverPath);
 
 async function runBmp() {
   await bmpClient.start();
   await bmpClient.createHar();
-  const driver = new webdriver.Builder()
+
+  const hostPort = 'localhost:' + bmpClient.proxy.port;
+  let chromeOptions = new Options();
+
+  // To collect all http request, need use chromeOptions --ignore-certificate-error
+  // setProxy() no need to set.
+  chromeOptions.addArguments(`--proxy-server=${hostPort}`)
+  chromeOptions.addArguments('--ignore-certificate-errors')
+
+  driver = new webdriver.Builder()
     .forBrowser('chrome')
-    .setProxy(selProxy.manual({ http: 'localhost:' + bmpClient.proxy.port }))
+    // .setProxy(selProxy.manual({ http: 'localhost:' + bmpClient.proxy.port }))
+    // .setChromeService(serviceBuilder)
+    .setChromeOptions(chromeOptions)
     .build();
 
 
   await driver.get("https://search.yahoo.com");
-  const harData = await bmpClient.getHar();
+  await driver.sleep(5000);
+
+  harData = await bmpClient.getHar();
   //do something
-  console.log(harData);
-  await bmpClient.end();
+  console.log("harData:", harData);
+
 }
 
 runBmp()
-  .then(function () {
+  .then(async function () {
+    console.log("harData.log.entries: ", harData.log.entries.length);
+    fs.writeFileSync('yahoo.har', JSON.stringify(harData) );
     console.log("Finished Successfully");
+    
+    await bmpClient.end();
+    await driver.quit();
     process.exit([0]);
   })
   .catch(function (err) {
